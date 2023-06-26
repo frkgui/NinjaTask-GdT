fb11bd36cd729398d64c191fb7f734156805fcd0
frkgui <78104494+frkgui@users.noreply.github.com>
2023-06-26 20:13:48 -0300

    config commit

--- src/br/com/ninjatask/Main.java
+++ src/br/com/ninjatask/Main.java
@@ -3,5 +3,7 @@ package br.com.ninjatask;
 public class Main {
     public static void main(String[] args) {
         System.out.println("papagaio");
+        System.out.println("aaaaaaaaaaa");
+
     }
 }
