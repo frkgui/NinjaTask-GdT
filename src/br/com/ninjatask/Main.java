07c8722c7136de00808824e0bb1b5951b666fdc3
carlosbernard0 <carlosbernardo774@gmail.com>
2023-06-26 21:15:41 -0300

    commit excloi

--- src/br/com/ninjatask/Main.java
+++ src/br/com/ninjatask/Main.java
@@ -2,9 +2,7 @@ package br.com.ninjatask;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("papagaio");
-        System.out.println("aaaaaaaaaaa");
-        System.out.println("Cleriton sávio");
+
 
     }
 }
